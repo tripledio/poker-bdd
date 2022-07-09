1245433b3e4f514b9ec03af1276739805a7fe523
Guido Dechamps <guido@tripled.io>
2022-07-09 10:50:13 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/test/kotlin/io/tripled/features/steps/TableSteps.kt

---
+++ src/test/kotlin/io/tripled/features/steps/TableSteps.kt
@@ -0,0 +1,20 @@
+package io.tripled.features.steps
+
+import io.cucumber.java.*
+import io.cucumber.java.en.*
+
+
+class TableSteps {
+    @Given("an empty Table")
+    fun an_empty_table() {
+        // Write code here that turns the phrase above into concrete actions
+        throw PendingException()
+    }
+
+    @Then("a person named {string} can join the Table")
+    fun a_person_named_can_join_the_table(string: String?) {
+        // Write code here that turns the phrase above into concrete actions
+        throw PendingException()
+    }
+
+}
---
+++ src/test/resources/features/start-a-game.feature
@@ -0,0 +1,5 @@
+Feature: Start a game
+
+  Scenario: Player can join a Table
+    Given an empty Table
+    Then a person named "Kris" can join the Table
